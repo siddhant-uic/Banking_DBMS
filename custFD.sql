bbf62cce024b40963f5df49be74fb7f4a8d8742d
Yuvraj Singh <yuvrajsingh808@gmail.com>
2022-02-27 00:23:25 +0530

    customer Fd tables

--- custFD.sql
+++ custFD.sql
@@ -1,136 +1,82 @@
-# ************************************************************
-# Sequel Ace SQL dump
-# Version 20029
-#
-# https://sequel-ace.com/
-# https://github.com/Sequel-Ace/Sequel-Ace
-#
-# Host: localhost (MySQL 8.0.27)
-# Database: bankingSystem
-# Generation Time: 2022-02-26 18:43:07 +0000
-# ************************************************************
-
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+--
+-- Host: localhost    Database: bankingsystem
+-- ------------------------------------------------------
+-- Server version	8.0.28
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-SET NAMES utf8mb4;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE='NO_AUTO_VALUE_ON_ZERO', SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+--
+-- Table structure for table `customer`
+--
 
-# Dump of table Account
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `Account`;
-
-CREATE TABLE `Account` (
-  `Account#` decimal(10,0) NOT NULL,
-  `Password` varchar(20) NOT NULL,
-  `Balance` decimal(15,2) NOT NULL,
-  PRIMARY KEY (`Account#`)
+DROP TABLE IF EXISTS `customer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `customer` (
+  `CustID` int NOT NULL,
+  `Password` varchar(45) NOT NULL,
+  `Income` float NOT NULL,
+  `Credit Score` float NOT NULL,
+  `AadharNo` decimal(12,0) unsigned NOT NULL,
+  PRIMARY KEY (`CustID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `customer`
+--
 
+LOCK TABLES `customer` WRITE;
+/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
+UNLOCK TABLES;
 
-# Dump of table AccountOpened
-# ------------------------------------------------------------
+--
+-- Table structure for table `fixeddeposits`
+--
 
-DROP TABLE IF EXISTS `AccountOpened`;
-
-CREATE TABLE `AccountOpened` (
-  `CustID` decimal(6,0) NOT NULL,
-  `BranchID` decimal(4,0) NOT NULL,
-  `Account#` decimal(10,0) NOT NULL,
-  `DateOfOpening` date NOT NULL,
-  PRIMARY KEY (`Account#`)
+DROP TABLE IF EXISTS `fixeddeposits`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `fixeddeposits` (
+  `DepositNo` int NOT NULL,
+  `TenureMonth` int NOT NULL,
+  `DateOfCreation` date NOT NULL,
+  `Amount` float NOT NULL,
+  `ROI` float NOT NULL,
+  PRIMARY KEY (`DepositNo`),
+  UNIQUE KEY `DepositNo_UNIQUE` (`DepositNo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `fixeddeposits`
+--
 
+LOCK TABLES `fixeddeposits` WRITE;
+/*!40000 ALTER TABLE `fixeddeposits` DISABLE KEYS */;
+/*!40000 ALTER TABLE `fixeddeposits` ENABLE KEYS */;
+UNLOCK TABLES;
 
-# Dump of table Branch
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `Branch`;
-
-CREATE TABLE `Branch` (
-  `branchID` decimal(4,0) NOT NULL,
-  `Locality` varchar(45) NOT NULL,
-  `City` varchar(45) NOT NULL,
-  `State` varchar(45) NOT NULL,
-  PRIMARY KEY (`branchID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-
-
-# Dump of table DepositoryAcc
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `DepositoryAcc`;
-
-CREATE TABLE `DepositoryAcc` (
-  `Account#` decimal(10,0) NOT NULL,
-  `InterestRate` decimal(2,0) DEFAULT NULL,
-  `minBalance` decimal(15,0) DEFAULT NULL,
-  `Type` varchar(45) NOT NULL,
-  PRIMARY KEY (`Account#`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-
-
-# Dump of table Employee
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `Employee`;
-
-CREATE TABLE `Employee` (
-  `empID` decimal(6,0) NOT NULL,
-  `Salary` int unsigned NOT NULL,
-  `DOJ` date NOT NULL,
-  `branchID` decimal(4,0) NOT NULL,
-  PRIMARY KEY (`empID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-
-
-# Dump of table LoanRequests
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `LoanRequests`;
-
-CREATE TABLE `LoanRequests` (
-  `requestID` decimal(6,0) NOT NULL,
-  `DateOfOpening` date NOT NULL,
-  `Amount` decimal(15,0) NOT NULL,
-  `DurationMonths` int NOT NULL,
-  `Type` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`requestID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-
-
-# Dump of table Manages
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `Manages`;
-
-CREATE TABLE `Manages` (
-  `empID` decimal(6,0) NOT NULL,
-  `branchID` decimal(4,0) NOT NULL,
-  `DOJ` date NOT NULL,
-  PRIMARY KEY (`empID`,`branchID`,`DOJ`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-
-
-# Dump of table person
-# ------------------------------------------------------------
+--
+-- Table structure for table `person`
+--
 
 DROP TABLE IF EXISTS `person`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `person` (
-  `AadharNo` int unsigned NOT NULL,
+  `AadharNo` decimal(12,0) unsigned NOT NULL,
   `FirstName` varchar(45) NOT NULL,
   `LastName` varchar(45) DEFAULT NULL,
   `DOB` date NOT NULL,
@@ -142,37 +88,49 @@ CREATE TABLE `person` (
   PRIMARY KEY (`AadharNo`),
   CONSTRAINT `person_chk_1` CHECK ((`Gender` in (_utf8mb4'M',_utf8mb4'F',_utf8mb4'O')))
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `person`
+--
 
 LOCK TABLES `person` WRITE;
 /*!40000 ALTER TABLE `person` DISABLE KEYS */;
-
-INSERT INTO `person` (`AadharNo`, `FirstName`, `LastName`, `DOB`, `Gender`, `HouseNo`, `Locality`, `City`, `State`)
-VALUES
-	(123,'abc','def','2000-10-20','M','1','1','1','1');
-
+INSERT INTO `person` VALUES (123,'abc','def','2000-10-20','M','1','1','1','1');
 /*!40000 ALTER TABLE `person` ENABLE KEYS */;
 UNLOCK TABLES;
 
-
-# Dump of table phonenumbers
-# ------------------------------------------------------------
+--
+-- Table structure for table `phonenumbers`
+--
 
 DROP TABLE IF EXISTS `phonenumbers`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `phonenumbers` (
   `PhoneNumber` int NOT NULL,
-  `AadharNo` int unsigned NOT NULL,
+  `AadharNo` decimal(12,0) unsigned NOT NULL,
   PRIMARY KEY (`PhoneNumber`,`AadharNo`),
-  KEY `FK_Person_idx` (`AadharNo`),
-  CONSTRAINT `FK_Person` FOREIGN KEY (`AadharNo`) REFERENCES `person` (`AadharNo`)
+  KEY `FK_Person_idx` (`AadharNo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `phonenumbers`
+--
 
+LOCK TABLES `phonenumbers` WRITE;
+/*!40000 ALTER TABLE `phonenumbers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `phonenumbers` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-02-27  0:23:04
