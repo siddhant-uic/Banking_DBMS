c7d945bc6226d289bb14828e088ac572868dd342
Yuvraj Singh <yuvrajsingh808@gmail.com>
2022-03-02 20:37:26 +0530

    LOAN ADDED

--- finalTablesWithData.sql
+++ finalTablesWithData.sql
@@ -18,7 +18,7 @@
 --
 -- Table structure for table `account`
 --
-
+use bank2;
 DROP TABLE IF EXISTS `account`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
